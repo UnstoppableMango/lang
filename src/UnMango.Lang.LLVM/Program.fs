b3d30b3d80370a87c36a3999217da8388041f124
Erik Rasmussen <erik.rasmussen@unmango.dev>
2025-02-06 13:09:32 -0600

    Move stuff around

--- src/UnMango.Lang.LLVM/Program.fs
+++ src/UnMango.Lang.LLVM/Program.fs
@@ -1,9 +1,10 @@
 ﻿open System
 open LLVMSharp
+open LLVMSharp.Interop
 open UnMango.Lang
 
 let ctx = LLVMContext()
-let m = Module()
+let m = LLVMModuleRef.CreateWithName("test")
 
 let v = IR.emitString ctx (Ast.String("test"))
 
