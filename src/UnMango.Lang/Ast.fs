3e530812cf1a5b2d83dcbde808d5376afb1b38a9
Erik Rasmussen <erik.rasmussen@unmango.dev>
2025-02-05 19:41:57 -0600

    Refactor AST and Parser in UnMango.Lang
    Significant changes have been made to the Abstract Syntax Tree (AST) and Parser in the UnMango.Lang module. The Node type has been simplified, removing abstract methods and introducing a new variant for strings. The Parser has also been updated to reflect these changes, with new parsers added for node lists and files. Error handling has been improved in the parse function by returning Result types instead of empty nodes on failure.

--- src/UnMango.Lang/Ast.fs
+++ src/UnMango.Lang/Ast.fs
@@ -1,9 +1,6 @@
 module UnMango.Lang.Ast
-
-type Node =
-    abstract Pos : unit -> int
-    abstract End : unit -> int
     
-type String = { Val: string }
+type Node =
+    | String of string
 
 type File = { Nodes: Node list }
--- src/UnMango.Lang/Parser.fs
+++ src/UnMango.Lang/Parser.fs
@@ -3,8 +3,14 @@ module UnMango.Lang.Parser
 open FParsec
 open UnMango.Lang.Ast
 
-let pstring: Parser<_, _> = pchar '"' >>. manySatisfy ((<>) '"') >>. pchar '"'
+let pstring: Parser<_, _> =
+    manySatisfy ((<>) '"') |> between (pchar '"') (pchar '"') |>> String
+
+let pnodelist: Parser<_, _> = many pstring
+
+let pfile: Parser<_, _> = pnodelist |>> (fun s -> { Nodes = s })
 
 let parse (input: string) =
-    let t = run pstring input
-    { Nodes = [] }
+    match run pfile input with
+    | Success(result, _, _) -> Result.Ok(result)
+    | Failure(msg, err, _) -> Result.Error(msg, err)
