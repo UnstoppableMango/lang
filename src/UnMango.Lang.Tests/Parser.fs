7775d865bcd6770d8e22a33d48bd48f53404aaf4
Erik Rasmussen <erik.rasmussen@unmango.dev>
2025-02-05 19:42:20 -0600

    Buncha stuff

--- src/UnMango.Lang.Tests/Parser.fs
+++ src/UnMango.Lang.Tests/Parser.fs
@@ -1,8 +1,23 @@
 module Parser
 
 open System
-open Xunit
+open FsCheck
+open FsCheck.FSharp
+open FsCheck.Xunit
+open Swensen.Unquote
+open UnMango.Lang
 
-[<Fact>]
-let ``My test`` () =
-    Assert.True(true)
+type String =
+    static member Double() =
+        ArbMap.defaults
+        |> ArbMap.arbitrary<NonEmptyString>
+        |> Arb.filter (fun (NonEmptyString s) -> s |> String.forall Char.IsLetterOrDigit)
+
+let parseSuccess =
+    function
+    | Ok(x) -> x
+    | Error(msg, _) -> failwith msg
+
+[<Property(Arbitrary = [| typeof<String> |])>]
+let ``Should parse a string`` (NonEmptyString s) =
+    test <@ Parser.parse ("\"" + s + "\"") |> parseSuccess = { Nodes = [ (Ast.String s) ] } @>
