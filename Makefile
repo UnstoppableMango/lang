0ad6785dc3f1db703d027e32e6e6ca3cd7925237
Erik Rasmussen <erik.rasmussen@unmango.dev>
2025-02-06 21:34:46 -0600

    Whole lotta stuff again

--- Makefile
+++ Makefile
@@ -1,6 +1,8 @@
 export GOBIN := ${CURDIR}/bin
 GO_PROJ      := github.com/unstoppablemango/lang
 
+DOTNET_RID ?= $(shell dotnet --info | grep RID | sed 's/RID\:\s*//g')
+
 LOCALBIN := ${CURDIR}/bin
 DEVCTL   := ${LOCALBIN}/devctl
 DPRINT   := ${LOCALBIN}/dprint
@@ -8,7 +10,7 @@ BUF      := ${LOCALBIN}/buf
 
 build: .make/dotnet-build bin/ir
 gen: .make/buf-gen
-test: .make/dotnet-test
+test: .make/dotnet-test .make/ginkgo-test
 format: .make/dprint-format
 tidy: go.sum
 dev: .envrc bin/devctl
@@ -28,19 +30,26 @@ go.mod:
 bin/ir: $(shell $(DEVCTL) list --go)
 	go build -o $@ -tags=llvm19 ./cmd/ir
 
+bin/lang-host: src/UnMango.Lang.Host/bin/lang-host
+	cp $< $@
+
 bin/devctl: .versions/devctl | bin
 	go install github.com/unmango/devctl/cmd@v$(shell cat $<)
 	mv bin/cmd $@
 
-bin/ginkgo: go.mod
+bin/ginkgo: go.mod | bin
 	go install github.com/onsi/ginkgo/v2/ginkgo
 
 bin/dprint: .versions/dprint | .make/dprint/install.sh bin
 	DPRINT_INSTALL=${CURDIR} .make/dprint/install.sh $(shell $(DEVCTL) v dprint)
 	@touch $@
 
-bin/buf: .versions/buf | bin
+bin/buf: .versions/buf | bin/devctl
 	go install github.com/bufbuild/buf/cmd/buf@$(shell $(DEVCTL) $<)
+
+src/UnMango.Lang.Host/bin/lang-host: $(shell $(DEVCTL) list --cs) | bin/devctl
+	dotnet publish src/UnMango.Lang.Host -p:DebugSymbols=false \
+	--use-current-runtime --self-contained --configuration Release --output $(dir $@)
 
 .make/dotnet-build: $(shell $(DEVCTL) list --dotnet) Lang.sln | .make bin/devctl
 	dotnet build
@@ -50,7 +59,11 @@ bin/buf: .versions/buf | bin
 	dotnet test
 	@touch $@
 
-.make/dprint/install.sh:
+.make/ginkgo-test: $(shell $(DEVCTL) list --go) | .make bin/devctl bin/ginkgo
+	ginkgo run -r ./
+	@touch $@
+
+.make/dprint/install.sh: | .make
 	mkdir -p $(dir $@)
 	curl -fsSL https://dprint.dev/install.sh -o $@
 	chmod +x $@
@@ -58,5 +71,5 @@ bin/buf: .versions/buf | bin
 .make/dprint-format: README.md .dprint.jsonc .github/renovate.json | bin/dprint
 	$(DPRINT) fmt
 
-.make/buf-gen:
+.make/buf-gen: | .make
 	$(BUF) generate
--- e2e_test.go
+++ e2e_test.go
@@ -1,14 +1,19 @@
 package lang_test
 
 import (
+	"bytes"
 	"context"
+	"crypto/tls"
+	"net"
 	"net/http"
 	"os/exec"
 
 	"connectrpc.com/connect"
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
+	"github.com/onsi/gomega/gbytes"
 	"github.com/onsi/gomega/gexec"
+	"golang.org/x/net/http2"
 
 	unmangov1alpha1 "github.com/unstoppablemango/lang/pkg/io/unmango/v1alpha1"
 	"github.com/unstoppablemango/lang/pkg/io/unmango/v1alpha1/unmangov1alpha1connect"
@@ -16,19 +21,27 @@ import (
 
 var _ = Describe("LangE2eSuite", func() {
 	var (
-		ses    *gexec.Session
-		client unmangov1alpha1connect.ParserServiceClient
+		ses     *gexec.Session
+		client  unmangov1alpha1connect.ParserServiceClient
+		hostlog *bytes.Buffer
 	)
 
 	BeforeEach(func() {
 		var err error
-		cmd := exec.Command("bin/host")
-		ses, err = gexec.Start(cmd, GinkgoWriter, GinkgoWriter)
+
+		By("Starting the language host")
+		hostlog = &bytes.Buffer{}
+		cmd := exec.Command("bin/lang-host")
+		ses, err = gexec.Start(cmd, hostlog, GinkgoWriter)
 		Expect(err).NotTo(HaveOccurred())
 
+		By("Waiting for the langugage host to start")
+		Eventually(ses.Out).Should(gbytes.Say("Now listening on: http://localhost:5000"))
+
+		By("Creating a parser client")
 		client = unmangov1alpha1connect.NewParserServiceClient(
-			http.DefaultClient,
-			"",
+			newInsecureClient(),
+			"http://localhost:5000",
 			connect.WithGRPC(),
 		)
 	})
@@ -43,6 +56,22 @@ var _ = Describe("LangE2eSuite", func() {
 	})
 
 	AfterEach(func() {
-		Eventually(ses.Terminate().Exited).Should(gexec.Exit(0))
+		By("Terminating the host")
+		Eventually(ses.Interrupt()).Should(gexec.Exit(0))
 	})
 })
+
+func newInsecureClient() *http.Client {
+	return &http.Client{
+		Transport: &http2.Transport{
+			AllowHTTP: true,
+			DialTLS: func(network, addr string, _ *tls.Config) (net.Conn, error) {
+				// If you're also using this client for non-h2c traffic, you may want
+				// to delegate to tls.Dial if the network isn't TCP or the addr isn't
+				// in an allowlist.
+				return net.Dial(network, addr)
+			},
+			// Don't forget timeouts!
+		},
+	}
+}
