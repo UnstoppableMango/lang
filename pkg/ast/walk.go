88820d602e30c60da288c762c0f6ef28d730c40e
Erik Rasmussen <erik.rasmussen@unmango.dev>
2025-02-12 13:34:30 -0600

    Maybe walk

--- pkg/ast/walk.go
+++ pkg/ast/walk.go
@@ -2,6 +2,7 @@ package ast
 
 import (
 	"context"
+	"net"
 	"os"
 
 	unmangov1alpha1 "github.com/unstoppablemango/lang/pkg/io/unmango/v1alpha1"
@@ -14,12 +15,30 @@ type Visitor interface {
 
 func Walk(ctx context.Context, v Visitor, node *unmangov1alpha1.Node) error {
 	sock, err := os.CreateTemp("", "")
+	if err != nil {
+		return err
+	}
+
+	lis, err := net.Listen("tcp", sock.Name())
+	if err != nil {
+		return err
+	}
 
 	server := grpc.NewServer()
 	unmangov1alpha1.RegisterVisitorServiceServer(server, NewServer(v))
+	go server.Serve(lis)
+	defer server.GracefulStop()
 
-	client := unmangov1alpha1.NewAstServiceClient(nil)
-	_, err := client.Walk(ctx, &unmangov1alpha1.WalkRequest{
+	conn, err := grpc.NewClient(sock.Name())
+	if err != nil {
+		return err
+	}
+
+	client := unmangov1alpha1.NewAstServiceClient(conn)
+	_, err = client.Walk(ctx, &unmangov1alpha1.WalkRequest{
+		Uri:  sock.Name(),
 		Node: node,
 	})
+
+	return err
 }
