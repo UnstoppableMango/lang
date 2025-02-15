1a2bfeb54bf4cca91cff153c166cc405b08a8783
Erik Rasmussen <erik.rasmussen@unmango.dev>
2025-02-15 13:54:47 -0600

    AST

--- pkg/ast/ast.go
+++ pkg/ast/ast.go
@@ -1,3 +1,44 @@
 package ast
 
+type Node interface{}
 
+type Expr interface {
+	Node
+	exprNode()
+}
+
+type NumExpr struct {
+	Value float64
+}
+
+func (*NumExpr) exprNode() {}
+
+type VarExpr struct {
+	Name string
+}
+
+func (*VarExpr) exprNode() {}
+
+type BinExpr struct {
+	Op       rune
+	LHS, RHS Expr
+}
+
+func (*BinExpr) exprNode() {}
+
+type CallExpr struct {
+	Callee string
+	Args   []Expr
+}
+
+func (*CallExpr) exprNode() {}
+
+type Proto struct {
+	Name string
+	Args []string
+}
+
+type Func struct {
+	Proto *Proto
+	Body  Expr
+}
