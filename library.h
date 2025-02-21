c10a495ee7d06f83cb38d596f0e3a7dad947a9b4
Erik Rasmussen <erik.rasmussen@unmango.dev>
2025-02-21 15:47:17 -0600

    Figuring out the C++ landscape

---
+++ library.h
@@ -0,0 +1,6 @@
+#ifndef LANG_LIBRARY_H
+#define LANG_LIBRARY_H
+
+void hello();
+
+#endif //LANG_LIBRARY_H
---
+++ library_test.cpp
@@ -0,0 +1,5 @@
+#include <gtest/gtest.h>
+
+TEST(LibraryTest, BasicAssertions) {
+  EXPECT_EQ(7 * 6, 42);
+}
